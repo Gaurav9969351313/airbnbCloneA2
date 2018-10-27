9fa45bdc5f7753a2e8bf88555ffd826c687d8e96
gauravtalele <gauravtalele@gmail.com>
2018-10-27 09:05:58 -0700

    rental module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes,RouterModule } from "@angular/router";
+import { RentalModule } from "./rental/rental.module";
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { TempComponent } from './temp/temp.component';
+import { FooterComponent } from './common/footer/footer.component';
+
+
+const routes:Routes = [
+  {path: "", component: RentalComponent },
+  {path: "temp", component: TempComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    // RentalComponent,
+    TempComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RentalModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
